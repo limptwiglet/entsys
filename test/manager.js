c0ad3cdaef5791c639a4914eed89b045922e69cb
Mark Gerrard <limptwiglet@gmail.com>
2013-03-28 10:02:15 +0000

    Added component class and added createEntity method

--- test/manager.js
+++ test/manager.js
@@ -4,7 +4,7 @@ var sinon = require('sinon');
 var Manager = require('../lib/manager');
 var System = require('../lib/system');
 
-describe('manager', function () {
+describe('Entity manager', function () {
     var manager;
 
     beforeEach(function () {
@@ -16,24 +16,54 @@ describe('manager', function () {
         expect(manager._ID).to.equal(0);
     });
 
-    describe('systems', function () {
-        it('should be able to add systems', function () {
-            expect(manager._systems).to.have.length(0);
+    it('should have a getComponentRef method for getting a components unique reference', function () {
+        var componentRef = 'flanger';
+        var component = {
+            _name: componentRef
+        };
 
-            manager.add(new System(), new System());
+        var ref = manager.getComponentRef(component);
 
-            expect(manager._systems).to.have.length(2);
-        });
+        expect(ref).to.equal(componentRef);
+    });
 
-        it('should call process on each system when process is called on manager', function () {
-            var system = new System();
-            var system2 = new System();
-            var spy = system2.process = system.process = sinon.spy();
+    it('should be able to create a new entity with or without a passed in id', function () {
+        var id = 10;
+        var entity = manager.createEntity();
+        var entity2 = manager.createEntity(id);
 
-            manager.add(system, system2);
-            manager.process();
+        expect(entity._id).to.equal(0);
+        expect(entity).to.not.be.undefined;
+        expect(entity2).to.not.be.undefined;
+        expect(entity2._id).to.equalt(id);
+    });
 
-            expect(spy.callCount).to.equal(2);
-        });
+    it('should be able to add systems', function () {
+        expect(manager._systems).to.have.length(0);
+
+        manager.add(new System(), new System());
+
+        expect(manager._systems).to.have.length(2);
+    });
+
+    it('should call process on each system when process is called on manager', function () {
+        var system = new System();
+        var system2 = new System();
+        var spy = system2.process = system.process = sinon.spy();
+
+        manager.add(system, system2);
+        manager.process();
+
+        expect(spy.callCount).to.equal(2);
+    });
+
+    it('should update systems entities when manager calls process method', function () {
+        var system = new System();
+
+        manager.add(system);
+
+        manager.process();
+
+        expect(system._entities.length).to.be.greater.than(1);
     });
 });
